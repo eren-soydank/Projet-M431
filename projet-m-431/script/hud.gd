bd707d5d8195e4c185c4690d11a33f315cdd9672
Marc-Antoine Bonvin <marc-antoine.bnvn@eduge.ch>
2026-04-13 19:40:36 +0200

    animation porte

--- projet-m-431/script/hud.gd
+++ projet-m-431/script/hud.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+# on recupere tous les coeur
 @onready var coeurs = [$coeur1, $coeur2, $coeur3, $coeur4, $coeur5, $coeur6, $coeur7, $coeur8, $coeur9, $coeur10]
+# on recupere le ver
 @onready var ver = $ver/AnimatedSprite2D
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# pour chaques coeur
 	for coeur in range(10):
+		# on commance l'annimation du coeur
 		coeurs[coeur].play("appear")
+	# on comance j'annimation du ver
 	ver.play("idle")
 
 
