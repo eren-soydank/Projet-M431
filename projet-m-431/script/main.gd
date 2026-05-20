e0a1ab8a34394094035635a95b5a9bd2edb4638f
Marc-Antoine Bonvin <marc-antoine.bnvn@eduge.ch>
2026-05-21 00:46:42 +0200

    parchment

--- projet-m-431/script/main.gd
+++ projet-m-431/script/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 # le niveau actuel
 const DOUBLE_JUMP_PAD_SCENE = preload("res://scènes/double_jump_pad.tscn")
 
-var curent_level = 3
+var curent_level = 1
 
 @onready var player = $player
 @onready var hud = $player/hud
